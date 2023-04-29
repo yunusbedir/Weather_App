6b4391083ff0430ccca101358b592f5fecc9b33e
yunusbedir <yunus.bedir@btc-ag.com.tr>
2023-04-29 11:03:41 +0300

    HomeScreen designed

--- androidApp/src/main/java/com/yunusbedir/weatherapp/android/ui/WeatherMain.kt
+++ androidApp/src/main/java/com/yunusbedir/weatherapp/android/ui/WeatherMain.kt
@@ -1,32 +1,20 @@
 package com.yunusbedir.weatherapp.android.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.LinearGradientShader
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.yunusbedir.weatherapp.android.GreetingView
 import com.yunusbedir.weatherapp.android.MyApplicationTheme
 import com.yunusbedir.weatherapp.android.ui.common.Home
-import com.yunusbedir.weatherapp.android.ui.common.WeatherAppBar
-import com.yunusbedir.weatherapp.android.ui.common.weatherAppDestinations
 import com.yunusbedir.weatherapp.android.ui.home.HomeScreen
 import com.yunusbedir.weatherapp.android.ui.home.HomeViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -39,26 +27,13 @@ fun WeatherMain(
     val systemUiController = rememberSystemUiController()
     val scaffoldState = rememberScaffoldState()
 
-    val isSystemDark = isSystemInDarkTheme()
-    val statusBarColor = if (isSystemDark) {
-        MaterialTheme.colors.primaryVariant
-    } else {
-        Color.Transparent
-    }
-
     SideEffect {
-        systemUiController.setStatusBarColor(color = statusBarColor, darkIcons = !isSystemDark)
+        systemUiController.isStatusBarVisible = false
     }
-
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = weatherAppDestinations.find {
-        backStackEntry?.destination?.route == it.route ||
-                backStackEntry?.destination?.route == it.routeWithArgs
-    } ?: Home
 
     Scaffold(
         scaffoldState = scaffoldState,
-        backgroundColor = Color.Transparent
+        backgroundColor = Color.Transparent,
     ) { innerPadding ->
         NavHost(
             navController = navController,
--- androidApp/src/main/java/com/yunusbedir/weatherapp/android/ui/home/HomeScreen.kt
+++ androidApp/src/main/java/com/yunusbedir/weatherapp/android/ui/home/HomeScreen.kt
@@ -1,15 +1,17 @@
 package com.yunusbedir.weatherapp.android.ui.home
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -18,10 +20,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.yunusbedir.weatherapp.android.R
 import com.yunusbedir.weatherapp.android.MyApplicationTheme
+import com.yunusbedir.weatherapp.android.R
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -41,6 +44,7 @@ fun HomeScreen(
             .background(color = Color.Transparent)
             .pullRefresh(state = pullRefreshState)
     ) {
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -48,14 +52,47 @@ fun HomeScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-
-            Text(
-                text = uiState.currentStation,
-                style = MaterialTheme.typography.h3,
-                color = Color.White,
+            Row(
                 modifier = Modifier
-                    .padding(top = 56.dp)
-            )
+                    .fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(56.dp)
+                        .padding(8.dp)
+                        .align(Alignment.CenterVertically)
+                        .clickable(MutableInteractionSource(), null) {
+                            //onDeleteClick()
+                        },
+                    tint = Color.White
+                )
+
+                Text(
+                    text = uiState.currentStation,
+                    style = MaterialTheme.typography.h4,
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(top = 8.dp),
+                    textAlign = TextAlign.Center
+                )
+
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(56.dp)
+                        .padding(8.dp)
+                        .clickable(MutableInteractionSource(), null) {
+                            //onDeleteClick()
+                        },
+                    tint = Color.White
+                )
+
+            }
 
             Image(
                 painter = painterResource(id = R.drawable.rain),
@@ -66,23 +103,50 @@ fun HomeScreen(
                     .padding(top = 48.dp)
             )
 
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(0.4f),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                Text(
-                    text = uiState.temp,
-                    style = MaterialTheme.typography.h3,
-                    color = Color.White
-                )
-                Text(
-                    text = uiState.unitType,
-                    style = MaterialTheme.typography.h6,
-                    color = Color.White
-                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = uiState.temp,
+                        style = MaterialTheme.typography.h3,
+                        color = Color.White
+                    )
+                    Text(
+                        text = uiState.unitType,
+                        style = MaterialTheme.typography.h6,
+                        color = Color.White
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = uiState.weatherDescription,
+                        style = MaterialTheme.typography.body2,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "${uiState.tempMin} - ${uiState.tempMax}",
+                        style = MaterialTheme.typography.body2,
+                        color = Color.White
+                    )
+                }
+
             }
 
             LazyRow(
